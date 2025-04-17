58804da75fcae00c2fa2abbbe2fe54d7f02adc13
ousmane <ousmanemamadiallo@gmail.com>
2025-04-17 22:08:10 +0000

    ajout code  fonction qui genere un grille de takuzu complete

--- Takuzubeta.R
+++ Takuzubeta.R
@@ -11,6 +11,33 @@ ligne_valide_simple <- function(ligne) {
   return(TRUE)
 }
 
+# Générateur de grille Takuzu complète
+
+generer_grille_complete_logique <- function() {
+  grille <- matrix(NA, nrow = taille_matrice, ncol = taille_matrice)
+  lignes_existantes <- list()
+  for (i in 1:taille_matrice) {
+    valide <- FALSE
+    essais <- 0
+    while (!valide && essais < 1000) {
+      ligne <- sample(rep(0:1, 3))
+      if (!ligne_valide_simple(ligne)) { essais <- essais + 1; next }
+      if (any(sapply(lignes_existantes, function(l) all(l == ligne)))) {
+        essais <- essais + 1; next
+      }
+      grille[i, ] <- ligne
+      lignes_existantes[[length(lignes_existantes)+1]] <- ligne
+      valide <- TRUE
+    }
+  }
+  for (j in 1:taille_matrice) {
+    col <- grille[, j]
+    if (!ligne_valide_simple(col) || sum(col == 0) != 3 ||
+        any(duplicated(apply(grille[, j, drop=FALSE], 2, paste, collapse="")))) {
+      return(generer_grille_complete_logique())
+    }
+  }
+
 
 
 
